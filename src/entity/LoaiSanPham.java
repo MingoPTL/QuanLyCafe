838a460c128204f2c2151c3431501f939c2a06f6
MingoPTL <djtmethangtam@gmail.com>
2025-11-11 21:08:40 +0700

    Them loai san pham

---
+++ src/entity/LoaiSanPham.java
@@ -0,0 +1,32 @@
+package entity;
+
+public class LoaiSanPham {
+	private String maloai;
+	private String tenloai;
+	public String getMaloai() {
+		return maloai;
+	}
+	public void setMaloai(String maloai) {
+		this.maloai = maloai;
+	}
+	public String getTenloai() {
+		return tenloai;
+	}
+	public void setTenloai(String tenloai) {
+		this.tenloai = tenloai;
+	}
+	public LoaiSanPham(String maloai, String tenloai) {
+		super();
+		this.maloai = maloai;
+		this.tenloai = tenloai;
+	}
+	public LoaiSanPham() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "LoaiSanPham [maloai=" + maloai + ", tenloai=" + tenloai + "]";
+	}
+	
+	
+}
--- src/entity/SanPham.java
+++ src/entity/SanPham.java
@@ -8,68 +8,62 @@ public class SanPham {
 	private double donGiaSanPham;
 	private NhaCungCap nhaCungCap;
 	private String moTaSanPham;
-	
-	// Constructor
-	public SanPham() {
-		super();
-	}
-
-	public SanPham(String maSanPham, String tenSanPham, double donGiaSanPham, NhaCungCap nhaCungCap,
-			String moTaSanPham) {
-		super();
-		this.maSanPham = maSanPham;
-		this.tenSanPham = tenSanPham;
-		this.donGiaSanPham = donGiaSanPham;
-		this.nhaCungCap = nhaCungCap;
-		this.moTaSanPham = moTaSanPham;
-	}
-
-	// Getter, setter
+	private LoaiSanPham loaiSanpham;
 	public String getMaSanPham() {
 		return maSanPham;
 	}
-
 	public void setMaSanPham(String maSanPham) {
 		this.maSanPham = maSanPham;
 	}
-
 	public String getTenSanPham() {
 		return tenSanPham;
 	}
-
 	public void setTenSanPham(String tenSanPham) {
 		this.tenSanPham = tenSanPham;
 	}
-
 	public double getDonGiaSanPham() {
 		return donGiaSanPham;
 	}
-
 	public void setDonGiaSanPham(double donGiaSanPham) {
 		this.donGiaSanPham = donGiaSanPham;
 	}
-
 	public NhaCungCap getNhaCungCap() {
 		return nhaCungCap;
 	}
-
 	public void setNhaCungCap(NhaCungCap nhaCungCap) {
 		this.nhaCungCap = nhaCungCap;
 	}
-
 	public String getMoTaSanPham() {
 		return moTaSanPham;
 	}
-
 	public void setMoTaSanPham(String moTaSanPham) {
 		this.moTaSanPham = moTaSanPham;
 	}
-
-	// Ghi đè
+	public LoaiSanPham getLoaiSanpham() {
+		return loaiSanpham;
+	}
+	public void setLoaiSanpham(LoaiSanPham loaiSanpham) {
+		this.loaiSanpham = loaiSanpham;
+	}
+	public SanPham(String maSanPham, String tenSanPham, double donGiaSanPham, NhaCungCap nhaCungCap, String moTaSanPham,
+			LoaiSanPham loaiSanpham) {
+		super();
+		this.maSanPham = maSanPham;
+		this.tenSanPham = tenSanPham;
+		this.donGiaSanPham = donGiaSanPham;
+		this.nhaCungCap = nhaCungCap;
+		this.moTaSanPham = moTaSanPham;
+		this.loaiSanpham = loaiSanpham;
+	}
+	public SanPham() {
+		super();
+	}
 	@Override
 	public String toString() {
 		return "SanPham [maSanPham=" + maSanPham + ", tenSanPham=" + tenSanPham + ", donGiaSanPham=" + donGiaSanPham
-				+ ", nhaCungCap=" + nhaCungCap + ", moTaSanPham=" + moTaSanPham + "]";
+				+ ", nhaCungCap=" + nhaCungCap + ", moTaSanPham=" + moTaSanPham + ", loaiSanpham=" + loaiSanpham + "]";
 	}
+	
+	
 	
 }
